e581dd5c3ccff8870fde2d68b7eb333837b27ed0
Simon Frost <s.frost@frostnet.co.uk>
2017-02-27 16:43:15 +0000

    Added module skeleton for http://alanstorm.com/magento_2_adding_frontend_files_to_your_module/

---
+++ app/code/AlanStorm/AddingFrontendFilesToYourModule/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @link http://alanstorm.com/magento_2_adding_frontend_files_to_your_module/
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::MODULE,
+	'AlanStorm_AddingFrontendFilesToYourModule',
+	__DIR__
+);?>
